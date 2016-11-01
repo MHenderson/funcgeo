40de0886bf79dc866da802fe953e2a0802a6b8e8
Matthew Henderson <matthew.james.henderson@gmail.com>
2016-11-01 14:35:38 -0400

    Make angle an argument of rot().

--- R/rot.R
+++ R/rot.R
@@ -1,9 +1,12 @@
-#' rot
+#' Rotate
+#'
+#' @param g grob
+#' @return A grob
 #' @export
-rot <- function(g) {
+rot <- function(g, angle = 90) {
   grid::editGrob(g, vp = viewport(
     width  = ifelse(is.null(g$vp$width), 0.5, g$vp$width),
     height = ifelse(is.null(g$vp$height), 0.5, g$vp$height),
-    angle  = ifelse(is.null(g$vp$angle), 90, g$vp$angle + 90))
+    angle  = ifelse(is.null(g$vp$angle), angle, g$vp$angle + angle))
   )
 }
