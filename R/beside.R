015a2cddd1e11e8edcdef8a6fff851b8b150528a
Matthew Henderson <matthew.james.henderson@gmail.com>
2016-11-01 14:36:08 -0400

    Change the order of arguments to the beside() function.

--- R/beside.R
+++ R/beside.R
@@ -1,5 +1,8 @@
-#' beside
+#' Beside
+#'
+#' @param g grob
+#' @return A grob
 #' @export
-beside <- function(m, n, p, q) {
+beside <- function(p, q, m = 1, n = 1) {
   gridExtra::arrangeGrob(p, q, ncol = 2, widths = c(n*m/(n + 1), m^2/(m + 1)))
 }
