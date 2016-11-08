8f190f1208875fb0972e32536d4769ce99b8f9c4
Matthew Henderson <matthew.james.henderson@gmail.com>
2016-11-08 15:10:59 -0500

    Improve documentation of the quartet function.

--- R/quartet.R
+++ R/quartet.R
@@ -1,8 +1,11 @@
 #' Quartet
 #'
-#' @param g grob
+#' @param p A picture
+#' @param q Another picture
+#' @param r Yet another picture
+#' @param s The last picture
 #' @return A grob
 #' @export
-quartet<- function(p, q, r, s) {
+quartet <- function(p, q, r, s) {
   return(above(beside(p, q), beside(r, s)))
 }
