eb28d5f4957c9f575e40a6fb8a84374ab9f95536
Matthew Henderson <matthew.james.henderson@gmail.com>
2016-12-31 08:31:05 -0500

    Rotate does not resize.

--- R/rot-grob.R
+++ R/rot-grob.R
@@ -5,8 +5,6 @@
 #' @export
 rot.grob <- function(g, angle = 90) {
   g <- grid::editGrob(g, vp = grid::viewport(
-    width  = ifelse(is.null(g$vp$width), 0.5, g$vp$width),
-    height = ifelse(is.null(g$vp$height), 0.5, g$vp$height),
     angle  = ifelse(is.null(g$vp$angle), angle, g$vp$angle + angle))
   )
   return(g)
