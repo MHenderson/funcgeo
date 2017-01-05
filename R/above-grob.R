a81a8880e0c943387c8daf4b68fa0c99552df74b
Matthew Henderson <matthew.james.henderson@gmail.com>
2017-01-05 09:21:32 -0500

    Fix proportions in above-grob.

--- R/above-grob.R
+++ R/above-grob.R
@@ -6,7 +6,7 @@
 #' @return A new grob obtained by superimposing p and q, one above the other.
 #' @export
 above.grob <- function(p, q, m = 1, n = 1) {
-  g <- gridExtra::arrangeGrob(p, q, ncol = 1, heights = c(n*m/(n + 1), m^2/(m + 1)))
+  g <- gridExtra::arrangeGrob(p, q, ncol = 1, heights = c(m/(m + n), n/(m + n)))
   class(g) <- c("picture", class(g))
   return(g)
 }
