777a00c0a42350218943f8a50574cbc9035ff893
Matthew Henderson <matthew.james.henderson@gmail.com>
2017-01-22 06:08:22 -0500

    Make rot a one-liner.

--- R/rot-grob.R
+++ R/rot-grob.R
@@ -4,8 +4,7 @@
 #' @return Rotated grob
 #' @export
 rot.grob <- function(g, angle = 90) {
-  g <- grid::editGrob(g, vp = grid::viewport(
+  grid::editGrob(g, vp = grid::viewport(
     angle  = ifelse(is.null(g$vp$angle), angle, g$vp$angle + angle))
   )
-  return(g)
 }
