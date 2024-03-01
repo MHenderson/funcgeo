c3615086808ff8485da94c110503e961b1e38045
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-03-01 10:43:47 +0000

    Remove all grob data.

--- data-raw/f.R
+++
@@ -1,9 +0,0 @@
-f <- linesGrob(
-  x = c(.1, .9, .9, .2, .2, .6, .6, .2, .2, .1, .1),
-  y = c(.9, .9, .8, .8, .6, .6, .5, .5, .1, .1, .9),
-  vp = viewport(width = 1, height = 1)
-)
-
-class(f) <- c("picture", class(f))
-
-save(f, file = "data/f.rda")
