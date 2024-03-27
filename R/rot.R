17f66ea04ea48a3cb832b6f9ad78cab0ad857721
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-03-27 09:47:31 +0000

    Document the rot function. (#40)

--- R/rot.R
+++ R/rot.R
@@ -1,8 +1,12 @@
-#' Rotate a grob.
+#' Rotate a picture
 #'
-#' @param g A grob
+#' `rot` returns a new picture, made by rotating the input picture. If no angle
+#' is specified then the new picture will be rotated 90 degrees. A different
+#' angle can be specified with the `angle` argument.
+#'
+#' @param g A `grid::grob` picture
 #' @param angle An angle
-#' @return Rotated grob
+#' @return A rotated `grid::grob` picture
 #' @export
 rot <- function(g, angle = 90) {
   grid::editGrob(g, vp = grid::viewport(
