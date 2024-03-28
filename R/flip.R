30ed16834e6981add4c82b7608d4eb046ede40ac
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-03-28 09:41:49 +0000

    Improved docs for the flip function. (#41)

--- R/flip.R
+++ R/flip.R
@@ -1,7 +1,10 @@
-#' Flip a grob.
+#' Flip a picture
 #'
-#' @param g A grob
-#' @return Flipped grob
+#' `flip` returns a new picture made by flipping the input picture along
+#' its vertical centre axis.
+#'
+#' @param g A `grid::grob` picture
+#' @return A flipped `grid::grob` picture
 #' @export
 flip <- function(g) {
   grid::editGrob(g, x = grid::unit(1, "npc") - g$x)
