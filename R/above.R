17576265879920c58f4b99bfcdef275e49395aca
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-03-28 09:52:42 +0000

    Improve docs for the above function.

--- R/above.R
+++ R/above.R
@@ -1,10 +1,15 @@
-#' Superimpose two grobs, one above the other.
+#' Superimpose two pictures, one above the other.
 #'
-#' @param p A grob
-#' @param q Another grob
+#' `above` returns a new picture made by placing the first input picture
+#' above the second input picture. The user can also provide arguments `m`
+#' and `n` in which case the pictures will be placed so that they occupy the
+#' output picture in a m:n ratio.
+#'
+#' @param p A `grid::grob` picture
+#' @param q Another `grid::grob` picture
 #' @param m An integer
 #' @param n Another integer
-#' @return A new grob obtained by superimposing p and q, one above the other.
+#' @return A new `grid::grob` picture obtained by superimposing p and q, one above the other.
 #' @export
 above <- function(p, q, m = 1, n = 1) {
   fg <- grid::frameGrob(layout = grid::grid.layout(2, 1))
