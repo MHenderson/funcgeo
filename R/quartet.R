a432a368669b88162486408a6df75aa0c6496712
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-03-29 09:18:59 +0000

    Improve docs for the quartet function. (#44)

--- R/quartet.R
+++ R/quartet.R
@@ -1,10 +1,15 @@
-#' Quartet
+#' Create a 2 by 2 grid pattern of pictures
 #'
-#' @param p A grob
-#' @param q Another grob
-#' @param r Yet another grob
-#' @param s The last grob
-#' @return A grob
+#' `quartet` returns a 2 by 2 grid pattern made up of the four input `grid::grob`
+#' pictures: `p`, `q`, `r` and `s`. In the final grid `p` is in the top-left
+#' position, `q` is in the top-right position, `r` is in the bottom-left
+#' position and `s` is in the top-right position.
+#'
+#' @param p A `grid::grob` for the top-left position
+#' @param q A `grid::grob` for the top-right position
+#' @param r A `grid::grob` for the bottom-left position
+#' @param s A `grid::grob` for the bottom-right position
+#' @return A `grid::grob` made up of the four input pictures arranged in a 2 by 2 grid pattern.
 #' @export
 quartet <- function(p, q, r, s) {
   return(above(beside(p, q), beside(r, s)))
