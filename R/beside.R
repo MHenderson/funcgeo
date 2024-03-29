3fd5c362839e20b7b391cb0055caee145d819113
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-03-29 09:05:33 +0000

    Improved docs for the beside function. (#43)

--- R/beside.R
+++ R/beside.R
@@ -1,10 +1,15 @@
-#' Superimpose two grobs side-by-side.
+#' Superimpose two pictures side-by-side.
 #'
-#' @param p grob
-#' @param q grob
+#' `beside` returns a new picture made by placing the first input picture
+#' beside the second input picture. The user can also provide arguments `m`
+#' and `n` in which case the pictures will be placed so that they occupy the
+#' output picture in a m:n ratio.
+#'
+#' @param p A `grid::grob` picture
+#' @param q Another `grid::grob` picture
 #' @param m An integer
 #' @param n Another integer
-#' @return A grob
+#' @return A new `grid::grob` picture obtained by superimposing p and q, one beside the other.
 #' @export
 beside <- function(p, q, m = 1, n = 1) {
   fg <- grid::frameGrob(layout = grid::grid.layout(1, 2))
