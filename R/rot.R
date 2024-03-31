277c4aa472a80a45b2c54f81c5a741f65d750c8d
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-03-31 14:48:21 +0000

    Minor clarification for the rot documentation.

--- R/rot.R
+++ R/rot.R
@@ -1,8 +1,8 @@
 #' Rotate a picture
 #'
 #' `rot` returns a new picture, made by rotating the input picture. If no angle
-#' is specified then the new picture will be rotated 90 degrees. A different
-#' angle can be specified with the `angle` argument.
+#' is specified then the new picture will be rotated clockwise by 90 degrees. A
+#' different angle can be specified with the `angle` argument.
 #'
 #' @param g A `grid::grob` picture
 #' @param angle An angle
